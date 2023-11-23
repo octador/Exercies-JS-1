955e5726d86aa0c1f0a9fbbf176b590624e063b7
octador <mutuon.j@gmail.com>
2023-11-24 00:32:08 +0100

    exer11

---
+++ js/Exercice_11.js
@@ -0,0 +1,8 @@
+let nmbr = prompt("saisir un nombre entre 1 et 10")
+if (nmbr>= 6) {
+    alert(nmbr + " est supérieur a 5 ")
+} else if(nmbr<=4) {
+    alert(nmbr + " est inférieur 5")
+}else{
+    alert( " le nombre est égal a 5")
+}
