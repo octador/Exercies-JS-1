eba7425e87bc724812bdc43793a7fbc270986079
octador <mutuon.j@gmail.com>
2023-11-23 16:44:34 +0100

    exe6

---
+++ js/Exercice_6.js
@@ -0,0 +1,10 @@
+function resteDivision(nombre1, nombre2) {
+    let resulta = nombre1 % nombre2
+    return resulta
+    
+}
+
+let nmb1  =  prompt("nombre 1");
+let nmb2 =prompt("nombre 2");
+
+alert(resteDivision(nmb1,nmb2)+" est le reste de la division")
