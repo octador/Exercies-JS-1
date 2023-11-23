0688af94f8f2365d64014efc081bf386e6f6d46d
octador <mutuon.j@gmail.com>
2023-11-23 16:25:17 +0100

    exe5

---
+++ js/Exercice_5.js
@@ -0,0 +1,9 @@
+let nmb1 = prompt("donne moi un nombre a virgule")
+let nmb2 = prompt("donne moi un deuxieme nombre a virgule")
+
+let nmb4 = Math.trunc(nmb1)
+let nmb5 = Math.trunc(nmb2)
+
+let result = nmb4*nmb5;
+
+alert(result)
