ad9b194fee1cc35007358071a1f3d26a17e5a9ab
octador <mutuon.j@gmail.com>
2023-11-23 16:13:21 +0100

    exe4

---
+++ js/Exercice_4.js
@@ -0,0 +1,5 @@
+let lastname = prompt("quel est ton prénom\n");
+let firstname = prompt("quel est ton nom de famille\n");
+let city = prompt("ou habite tu\n");
+
+alert(lastname+"\n" + firstname +"\n"+ city);
