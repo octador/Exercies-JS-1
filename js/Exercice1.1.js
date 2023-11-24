305f944586441f55c33c8f9611dffe5211f0f093
octador <mutuon.j@gmail.com>
2023-11-24 11:10:02 +0100

    exe1.1

---
+++ js/Exercice1.1.js
@@ -0,0 +1,19 @@
+// let etoiles = ["*"];
+// let etoile = ["*"];
+
+// for (let index = 1; index <= 5; index++) {
+//     let star = etoiles * index.length;
+//     for (let i = star; i < star.length; i++) {
+//         let star2 = etoile * star.length;
+//         starwars = star, star2;
+//         return starwars;
+//     }
+
+// }
+// console.log(starwars);
+
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log('*'.repeat(i));
+}
